5c6e73084964165c8fa1468d3359ba9cc84b5ce7
Piercarlo Serena <pierfreeman@gmail.com>
2014-12-03 16:56:15 +0000

    first code + dataset commit

---
+++ src/JSONEncoder.java
@@ -0,0 +1,8 @@
+import org.json.simple.JSONObject;
+
+/**
+ * Created by piercarloserena on 03/12/14.
+ */
+public class JSONEncoder {
+
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+/**
+ * Created by piercarloserena on 03/12/14.
+ */
+public class Main {
+
+
+}
